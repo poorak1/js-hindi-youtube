7e52fd77e0f1b1f2b78147a52fc09272b7450b08
Poorak Pasrija <132227473+poorak1@users.noreply.github.com>
2025-06-25 18:18:46 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -8,9 +8,14 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); // outputs false
+console.log(null == 0); // outputs false
+console.log(null >= 0); // outputs true
+
+/*
+Equality and comparison operators work differently in js.
+Comparison operators convert null to a number, treating it as 0
+*/
 
 console.log(undefined == 0);
 console.log(undefined > 0);
