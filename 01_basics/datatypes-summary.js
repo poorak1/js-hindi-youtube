cd1fbe5d1f9a3d1aef64e794214eccb23817b02e
Poorak Pasrija <132227473+poorak1@users.noreply.github.com>
2025-06-26 00:11:30 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -22,7 +22,7 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+const heros = ["shaktiman", "naagraj", "dogesh"];
 let myObj = {
     name: "hitesh",
     age: 22,
@@ -35,3 +35,41 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack and heap memory usage
+// Stack (Primitive), Heap(Non-Primitive)
+// Primitive data types provide a copy while non-primitive data-types provide a reference
+
+/*
+let myytname = "helloboi";
+let another_name = myytname;
+
+another_name = "hi"
+
+console.log(myytname); outputs => helloboi
+console.log(myytname); outputs => hi
+
+## A Copy of myytname is assigned to another_name so myytname remains unchanged while changing another_name
+*/
+
+/*
+let user_one = {
+    name: "suresh",
+    gender: "Male"
+}
+
+let user_two = user_one
+
+user_two.name = "sita"
+
+console.log(user_one);
+console.log(user_two);
+
+## Changing user_two data will affect user_one
+*/
+
+
+
+
